29727778c63b625b20c4f55792cccc7e056a7afa
Tintin <b07901031@ntu.edu.tw>
2022-01-13 13:36:01 +0800

    left right added

--- jetson.py
+++ jetson.py
@@ -29,9 +29,19 @@ def MotorForward(Motor):
     return
 
 def MotorLeftward(Motor):
+    Motor.MotorRun(0, 'backward', 100)
+    Motor.MotorRun(1, 'backward', 100)
+    time.sleep(0.01)
+    Motor.MotorStop(0)
+    Motor.MotorStop(1)
     return
 
 def MotorRightward(Motor):
+    Motor.MotorRun(0, 'forward', 100)
+    Motor.MotorRun(1, 'forward', 100)
+    time.sleep(0.01)
+    Motor.MotorStop(0)
+    Motor.MotorStop(1)
     return
 
 def MotorBackward(Motor):
