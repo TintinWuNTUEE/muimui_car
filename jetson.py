06a205b8a476d89f745d707cea5f6f42004d15fc
GinGerBread-Yellow <andyhuang0323@gmail.com>
2022-01-13 13:51:19 +0800

    feat: gstreamer

--- jetson.py
+++ jetson.py
@@ -4,6 +4,9 @@ import logging
 import threading
 import argparse
 import asyncio
+import cv2
+import argparse
+import multiprocessing as mp
 
 from aiortc import RTCPeerConnection, RTCSessionDescription, RTCConfiguration, RTCIceServer
 from aiortc.contrib.signaling import object_to_string, object_from_string
@@ -101,17 +104,80 @@ async def main(pc,Motor):
     await step2_running_loop()
 
 
+
+
+
+
+def gstreamer_camera(queue):
+    pipeline = (
+        "nvarguscamerasrc ! "
+            "video/x-raw(memory:NVMM), "
+            "width=(int)1920, height=(int)1080, "
+            "format=(string)NV12, framerate=(fraction)30/1 ! "
+        "queue ! "
+            "nvvidconv flip-method=2 ! "
+                "video/x-raw, "
+                "width=(int)1920, height=(int)1080, "
+                "format=(string)BGRx, framerate=(fraction)30/1 ! "
+            "videoconvert ! "
+                "video/x-raw, format=(string)BGR ! "
+            "appsink"
+        )
+
+    cap = cv2.VideoCapture(pipeline, cv2.CAP_GSTREAMER)
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            break
+        queue.put(frame)
+        print("[CAM] READ")
+
+
+def gstreamer_rtmpstream(queue):
+    pipeline = (
+        "appsrc ! "
+            "video/x-raw, format=(string)BGR ! "
+        "queue ! "
+            "videoconvert ! "
+                "video/x-raw, format=RGBA ! "
+            "nvvidconv ! "
+            "nvv4l2h264enc bitrate=8000000 ! "
+            "h264parse ! "
+            "flvmux ! "
+            'rtmpsink location="rtmp://localhost/rtmp/live live=1"'
+        )
+
+    writer = cv2.VideoWriter(pipeline, cv2.CAP_GSTREAMER, 0, 30.0, (1920, 1080))
+    while True:
+        frame = queue.get()
+        if frame is None:
+            break
+        writer.write(frame)
+        print("[RTMP] WRITE")
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("-v", "--verbose", action='store_true')
     args = vars(parser.parse_args())
     VERBOSE = args['verbose']
     Motor = MotorDriver()
+    
     # Run main event loop
     pc = RTCPeerConnection()
     coro = main(pc,Motor)
+    # === streaming ===
+    queue = mp.Queue(maxsize=1)
+    reader = mp.Process(target=gstreamer_camera, args=(queue,))
+    reader.start()
+    writer = mp.Process(target=gstreamer_rtmpstream, args=(queue,))
+    writer.start()
     try:
         loop = asyncio.get_event_loop()
         loop.run_until_complete(coro)
+        reader.join()
+        writer.join()
+
     except KeyboardInterrupt:
-        pass
+        reader.terminate()
+        writer.terminate()
